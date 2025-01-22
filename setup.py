e76552a61cdb6b916b2d6eb600a64da66021297e
pallavi-1998 <97725052+pallavi-1998@users.noreply.github.com>
2025-01-22 14:02:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ def get_requirements(file_path:str) -> List[str]:
 setup(
     name='RegressorProject',
     version='0.0.1',
-    author='Pallavi',
-    author_email='pp01.mcmt@gmail.com',
+    author='Your name',
+    author_email='yourgmail@gmail.com',
     install_requires= get_requirements('requirements.txt'),
     packages=find_packages()
 
